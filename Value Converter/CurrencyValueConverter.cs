9ecf60629a6f90e28c052abf4f4aab1ba47ae9f5
goduwohu <105153086+goduwohu@users.noreply.github.com>
2022-05-11 22:47:33 +0200

    Add files via upload

---
+++ Value Converter/CurrencyValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MVVM.Value_Converter
+{
+    public class CurrencyValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            decimal val = (decimal)value;
+
+            string test = parameter != null ? parameter.ToString() : "";
+
+            return val.ToString("0.00 ") + test;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string val = value.ToString();
+
+            if (parameter == null)
+                val = val.Trim();
+            else
+                val = val.Replace(parameter.ToString(), "").Trim();
+
+            decimal res = decimal.Parse(val);
+            return res;
+        }
+    }
+}
---
+++ Value Converter/EmailConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace MVVM.Value_Converter
+{
+    public class EmailConverter : IValueConverter
+    {
+        const string PREFIX = "mailto";
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string email = value as string;
+            if (!String.IsNullOrEmpty(email))
+                return $"{PREFIX}{email}";
+            return String.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string mailLink = value as string;
+            if (!string.IsNullOrEmpty(mailLink) && mailLink.ToLower().StartsWith(PREFIX))
+                return mailLink.Substring(PREFIX.Length, mailLink.Length - PREFIX.Length);
+            return mailLink ?? string.Empty;
+        }
+    }
+}
